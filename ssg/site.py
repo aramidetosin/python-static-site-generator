15ac7009b62e2a3cc9443c09502c9f1eb5d4a3b4
Tosin <aoluwatosin10@gmail.com>
2022-04-24 15:27:22 +0100

    update

--- ssg/site.py
+++ ssg/site.py
@@ -3,14 +3,14 @@ import glob
 
 class Site:
     def __init__(self, source, dest):
-        self.source = Path()
-        self.dest = Path()
+        self.source = Path(source)
+        self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = f"{self.dest}/{self.source.relative_to()}"
+        directory = f"{self.dest}/{path.relative_to(self.source)}"
         Path(directory).mkdir(parents=True, exist_ok=True)
     def build(self):
-        Path(self.dest).mkdir(parents=True, exist_ok=True)
+        self.dest.mkdir(parents=True, exist_ok=True)
         for i in self.source.rglob("*"):
             if i.is_dir():
                 self.create_dir(i)
